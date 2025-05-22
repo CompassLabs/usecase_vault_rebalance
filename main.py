27f8041cae769f82c73d04a37405e5d40e5f2517
Lukas Scheucher <scheuclu@gmail.com>
2025-05-22 09:41:07 +0100

    Working as soon as SDK is fixed.

--- main.py
+++ main.py
@@ -9,15 +9,17 @@ import os
 load_dotenv()
 st.set_page_config(layout="wide")
 
-st.title("Compass vault rebalance demo")
-address = st.text_input(label="Address", value='0xa829B388A3DF7f581cE957a95edbe419dd146d1B')
+st.title("Vault rebalance demo")
+st.text("""Demoing rebalaning USDC over several vaults.
+We are assuming all depoists are made from the same address, but that could easily be changed.
+""")
+address = st.text_input(
+    label="Choose the deposit address",
+    value="0xa829B388A3DF7f581cE957a95edbe419dd146d1B",
+)
 chain = st.selectbox(
     label="Chain", options=[Chain.ETHEREUM_MAINNET, Chain.ARBITRUM_MAINNET]
 )
-
-TOTAL = 1.1e6
-
-ORIGINAL_SPLIT = [0.41, 0.05, 0.28, 1 - 0.41 - 0.05 - 0.28]
 
 
 def horizontal_bar(container, value: float, color: str = "#4CAF50"):
@@ -56,6 +58,9 @@ user_positions = compass.morpho.user_position(
     user_address=address,
 ).vault_positions
 
+user_positions = [
+    position for position in user_positions if position.vault.asset.symbol == "USDC"
+]
 
 cols = st.columns(3)
 
@@ -66,14 +71,10 @@ with cols[0]:
         c.markdown(f"#### {position.vault.name}")
         c.text(position.vault.address)
         c.markdown(f"**{round(float(position.state.assets_usd), 2)}** USD deposits")
-        # c.progress(
-        #     ORIGINAL_SPLIT[i],
-        #     text=f"{round(ORIGINAL_SPLIT[i] * 100, 2)} % | {round(TOTAL * ORIGINAL_SPLIT[i], 2)} USDC",
-        # )
+        c.markdown(f"**{round(float(position.vault.daily_apys.apy) * 100, 2)} %** APY")
+
     deposits_arr = [pos.state.assets_usd for pos in user_positions]
-    vault_names = [
-        pos.vault.name for pos in user_positions
-    ]  # position.vault.asset.name
+    vault_names = [pos.vault.name for pos in user_positions]
     vault_symbol = [position.vault.asset.name for pos in user_positions]
     trace = go.Pie(
         labels=vault_names,
@@ -86,75 +87,30 @@ with cols[1]:
     st.subheader("Target Distribution")
     sliders = []
     for i, position in enumerate(user_positions):
-        # st.markdown(
-        #     """
-        #     <div style="height: 200px; border: 1px solid gray; padding: 10px;">
-        #         <p>This container has a fixed height of 200px.</p>
-        #         <p>You can add more content here.</p>
-        #     </div>
-        #     """,
-        #     unsafe_allow_html=True,
-        # )
         c = st.container(border=True, height=200)
-
         c.markdown(f"#### {position.vault.name}")
-        # c.text(position.vault.address)
         value = c.slider(
-            key=f"slider_{position.vault.address}", label="Choose percentage"
+            key=f"slider_{position.vault.address}",
+            label="Choose percentage",
+            value=100 if i == 0 else 0,
         )
         sliders.append(value)
 
-    # sliders=[st.slider(key=f'slider_{pos.vault.address}', label='aaa') for pos in user_positions]
     sum = sum(sliders)
     if sum != 100:
         st.warning("Rebalance percentages need to add up to 100%")
     else:
         st.success("This is a success message!", icon="✅")
-        st.button("Rebalance")
+        bt1 = st.button("Rebalance")
 
-with cols[2]:
-    st.subheader("Required Code")
-    st.code("""
-with cols[2]:
-    st.subheader("Desired State")
-    st.markdown(f"**TOTAL:** {TOTAL} USDC")
-    for i, vault in enumerate(vaults):
-        c = st.container(border=True)
-        c.markdown(f"#### {vault.name}")
-        c.text(vault.address)
-        c.progress(
-            ORIGINAL_SPLIT[i],
-            text=f"{round(ORIGINAL_SPLIT[i] * 100, 2)} % | {round(TOTAL * ORIGINAL_SPLIT[i], 2)} USDC",
+    with cols[2]:
+        st.subheader("Batched Transaction")
+        st.text(
+            "Below if the full code to make the rebalance happen programticallyusing the comopass SDK"
         )
-""")
 
-
-# changes = [0, 0, 0, 0]
-#
-# with cols[1]:
-#     st.subheader("Change")
-#     st.markdown(f"**TOTAL:** {TOTAL} USDC")
-#     for i, vault in enumerate(vaults):
-#         c = st.container(border=True)
-#         c.markdown(f"#### {vault.name}")
-#         c.text(vault.address)
-#         changes[i] = c.number_input(
-#             label=f"Change {i}",
-#             min_value=-TOTAL * ORIGINAL_SPLIT[i],
-#             max_value=TOTAL - TOTAL * ORIGINAL_SPLIT[i],
-#             value=0.0,
-#             step=1.0,
-#         )
-#
-#
-# with cols[2]:
-#     st.subheader("Desired State")
-#     st.markdown(f"**TOTAL:** {TOTAL} USDC")
-#     for i, vault in enumerate(vaults):
-#         c = st.container(border=True)
-#         c.markdown(f"#### {vault.name}")
-#         c.text(vault.address)
-#         c.progress(
-#             ORIGINAL_SPLIT[i],
-#             text=f"{round(ORIGINAL_SPLIT[i] * 100, 2)} % | {round(TOTAL * ORIGINAL_SPLIT[i], 2)} USDC",
-#         )
+        with open("./rebalance.py", "r") as f:
+            t = f.read()
+        t = t.split("# CODE START")[1].split("# CODE END")[0]
+        t = f"target_percentages={[i / 100 for i in sliders]}\n" + t
+        st.code(t)
---
+++ rebalance.py
@@ -0,0 +1,89 @@
+from compass_api_sdk import CompassAPI
+from compass_api_sdk.models import (
+    Chain,
+    MorphoVault,
+    UserOperation,
+    MulticallActionType,
+    MorphoWithdrawParams,
+    MorphoDepositParams,
+    IncreaseAllowanceParams,
+    IncreaseAllowanceParamsContractName,
+    MorphoSetVaultAllowanceParams,
+    # IncreaseAllowanceParamsContract = IncreaseAllowanceParams
+)
+from dotenv import load_dotenv
+import os
+from decimal import Decimal
+from eth_account import Account
+
+load_dotenv()
+
+CHAIN = Chain.ETHEREUM_MAINNET
+ADDRESS = "0xa829B388A3DF7f581cE957a95edbe419dd146d1B"
+
+
+compass = CompassAPI(
+    api_key_auth=os.environ.get("COMPASS_KEY"),
+)
+
+
+address2vault: dict[str, MorphoVault] = {
+    vault.address for vault in compass.morpho.vaults().vaults
+}
+user_positions = compass.morpho.user_position(
+    chain=CHAIN,
+    user_address=ADDRESS,
+).vault_positions
+user_positions = [
+    position for position in user_positions if position.vault.asset.symbol == "USDC"
+]
+
+deposits_arr = [Decimal(pos.state.assets_usd) for pos in user_positions]
+account = Account.from_key(os.environ.get("PK"))
+unsigned_auth = compass.transaction_batching.authorization(chain=CHAIN, sender=ADDRESS)
+
+signed_auth = Account.sign_authorization(
+    unsigned_auth.model_dump(by_alias=True), os.environ.get("PK")
+)
+
+
+target_percentages = [0.3, 0.4, 0.3]
+# CODE START
+target_absolutes = [Decimal(p) * sum(deposits_arr) for p in target_percentages]
+actions = []
+for position in user_positions:
+    actions.append(
+        UserOperation(
+            action_type=MulticallActionType.MORPHO_WITHDRAW,
+            body=MorphoWithdrawParams(
+                vault_address=position.vault.address, amount="ALL", receiver=ADDRESS
+            ),
+        )
+    )
+for i, position in enumerate(user_positions):
+    actions.append(
+        UserOperation(
+            action_type=MulticallActionType.MORPHO_SET_VAULT_ALLOWANCE,
+            body=MorphoSetVaultAllowanceParams(
+                vault_address=position.vault.address, amount=target_absolutes[i]
+            ),
+        )
+    )
+    actions.append(
+        UserOperation(
+            action_type=MulticallActionType.MORPHO_DEPOSIT,
+            body=MorphoDepositParams(
+                vault_address=position.vault.address,
+                amount=target_absolutes[i],
+                receiver=ADDRESS,
+            ),
+        )
+    )
+
+compass.transaction_batching.execute(
+    chain=CHAIN,
+    sender=ADDRESS,
+    signed_authorization=signed_auth.model_dump(),
+    actions=actions,
+)
+# CODE END
